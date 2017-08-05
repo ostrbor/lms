be19c339e5c87ababaf5dd5a18ce1fca98e64a9e
ostrbor <ostrbor@gmail.com>
2017-08-05 12:25:04 +0300

    Test signals

--- lms/auction/tests.py
+++ lms/auction/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from unittest.mock import patch
+from auction.models import Auction, User
+from datetime import datetime
 
-# Create your tests here.
+
+class SignalsTestCase(TestCase):
+    @patch('auction.signals.auction.send')
+    def test_auction_signal_triggered(self, mock):
+        owner = User.objects.get(pk=1)
+        close_at = datetime.now()
+        auction = Auction(
+            title='title',
+            current_price=10,
+            price_step=1,
+            close_at=close_at,
+            owner=owner).create()
+        self.assertTrue(mock.called)
+        auction.current_price = 11
+        auction.save()
+        self.assertEqual(mock.call_count, 2)
