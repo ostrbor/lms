4e8240b9f3e9c75e2237c203eef80c5fee8ec32e
ostrbor <ostrbor@gmail.com>
2017-08-05 12:03:21 +0300

    Complete signal

--- lms/auction/signals.py
+++ lms/auction/signals.py
@@ -1,16 +1,14 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from auction.models import Auction, User
-from django.core import mail
-
-import logging
-
-logger = logging.getLogger(__name__)
+from auction.tasks import notify_new_bid, notify_open_auction
 
 
 @receiver(post_save, sender=Auction)
-def notify_users(sender, instance, **kwargs):
+def auction(sender, instance, created, **kwargs):
     emails = User.objects.filter(is_active=True).values_list(
         'email', flat=True)
-    results = mail.send_mass_mail(emails)
-    logger.debug(results)
+    if created:
+        notify_open_auction(instance.title, list(emails))
+    else:
+        notify_new_bid(instance.price, list(emails))
