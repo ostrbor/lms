82d25cec703ecd5c57760f65a81ed77cd0caffa9
ostrbor <ostrbor@gmail.com>
2017-08-05 18:31:02 +0300

    Fix bug with loading apps

--- fabfile.py
+++ fabfile.py
@@ -13,8 +13,8 @@ def run():
 @task
 def migrate():
     with lcd(BASE_DIR):
-        local('./manage.py makemigrations users')
         local('./manage.py makemigrations auction')
+        local('./manage.py makemigrations users')
         local('./manage.py migrate')
         # local('./manage.py loaddata ../resources/fixtures/site.json')
         # local('./manage.py loaddata ../resources/fixtures/superuser.json')
