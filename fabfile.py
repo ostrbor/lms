a07582414c1216285d0040f21e0124192d486b91
ostrbor <ostrbor@gmail.com>
2017-08-05 10:17:10 +0300

    Install fabric to easily run commands

---
+++ fabfile.py
@@ -0,0 +1,26 @@
+from fabric.api import local, task
+from fabric.context_managers import lcd
+
+BASE_DIR = 'lms'
+
+
+@task
+def runserver():
+    with lcd(BASE_DIR):
+        local('./manage.py runserver')
+
+
+@task
+def migrate():
+    with lcd(BASE_DIR):
+        local('./manage.py makemigrations users')
+        local('./manage.py makemigrations auction')
+        local('./manage.py migrate')
+        # local('./manage.py loaddata ../resources/fixtures/site.json')
+        # local('./manage.py loaddata ../resources/fixtures/superuser.json')
+        # local('./manage.py loaddata ../resources/fixtures/user.json')
+
+
+@task
+def shell():
+    local(f'python {BASE_DIR}/manage.py shell_plus --ipython')
