582cd65d495f7e2745ae9d9db8e4b1c0e9ca75fd
ostrbor <ostrbor@gmail.com>
2017-08-05 20:09:11 +0300

    Move signals into models to load it on app start

--- lms/auction/models.py
+++ lms/auction/models.py
@@ -1,3 +1,7 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from auction.tasks import notify_new_bid, notify_open_auction
+from rest_framework.authtoken.models import Token
 from django.db import models
 from django.contrib.auth import get_user_model
 
@@ -10,10 +14,12 @@ class Auction(models.Model):
     price_step = models.PositiveIntegerField()
     close_at = models.DateTimeField()
     owner = models.ForeignKey(
-        User, related_name='owner',
-        on_delete=models.CASCADE)
+        User, related_name='owner', on_delete=models.CASCADE)
     winner = models.ForeignKey(
-        User, null=True, blank=True, related_name='winner',
+        User,
+        null=True,
+        blank=True,
+        related_name='winner',
         on_delete=models.CASCADE)
     is_opened = models.BooleanField(default=True, db_index=True)
 
@@ -25,3 +31,21 @@ class Bid(models.Model):
     price = models.PositiveIntegerField()
     auction = models.ForeignKey(Auction)
     user = models.ForeignKey(User)
+
+
+# TODO: rename to 'notify_auction'
+@receiver(post_save, sender=Auction)
+def auction(sender, instance, created, **kwargs):
+    emails = User.objects.filter(is_active=True).values_list(
+        'email', flat=True)
+    # TODO: use delay to use async version of task
+    if created:
+        notify_open_auction(instance.title, list(emails))
+    else:
+        notify_new_bid(instance.title, instance.current_price, list(emails))
+
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
