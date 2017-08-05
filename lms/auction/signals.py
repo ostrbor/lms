8b351a1b90d333a278e19fd61455883c4398f85c
ostrbor <ostrbor@gmail.com>
2017-08-05 18:31:49 +0300

    Introduce signal to save token for user on post_save

--- lms/auction/signals.py
+++ lms/auction/signals.py
@@ -2,8 +2,10 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from auction.models import Auction, User
 from auction.tasks import notify_new_bid, notify_open_auction
+from rest_framework.authtoken.models import Token
 
 
+# TODO: rename to 'notify_auction'
 @receiver(post_save, sender=Auction)
 def auction(sender, instance, created, **kwargs):
     emails = User.objects.filter(is_active=True).values_list(
@@ -13,3 +15,9 @@ def auction(sender, instance, created, **kwargs):
         notify_open_auction(instance.title, list(emails))
     else:
         notify_new_bid(instance.title, instance.current_price, list(emails))
+
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
