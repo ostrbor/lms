4a6c4ff211dd2a841c10936f2fb342fe62de0c60
ostrbor <ostrbor@gmail.com>
2017-08-06 11:53:53 +0300

    Make url names more verbose

--- lms/lms/urls.py
+++ lms/lms/urls.py
@@ -5,11 +5,13 @@ from rest_framework.authtoken import views
 # TODO: comment first urlpattern (use for debugging)
 urlpatterns = [
     url(r'^', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^user/$', UserCreate.as_view(), name='user'),
-    url(r'^token/', views.obtain_auth_token, name='token'),
-    url(r'^auctions/$', AuctionList.as_view(), name='auction-list'),
+    url(r'^users/$', UserCreate.as_view(), name='create_user'),
+    url(r'^tokens/', views.obtain_auth_token, name='get_token'),
+    url(r'^auctions/$', AuctionList.as_view(), name='auction_list'),
     url(r'^auctions/(?P<pk>[0-9]+)/$',
         AuctionDetail.as_view(),
-        name='auction-detail'),
-    url(r'^auctions/(?P<pk>[0-9]+)/bids$', BidCreate.as_view(), name='bid'),
+        name='auction_detail'),
+    url(r'^auctions/(?P<pk>[0-9]+)/bids$',
+        BidCreate.as_view(),
+        name='create_bid'),
 ]
